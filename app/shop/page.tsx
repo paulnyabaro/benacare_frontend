2e9c74e520e538702d71d74146b935cd23206f53
paulnyabaro <paulnyabaro1@gmail.com>
2024-02-07 12:03:06 +0300

    Updated slider

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -11,10 +11,10 @@ const page = () => {
             <div className="flex align-items-center">
                 <div className="w-full lg:w-1/2 gap-x-32">
                   <h3>High-Demand Medical Equipment</h3>
-                  <h1>Get <span className="text-green">30%</span> OFF</h1>
+                  <h1>Get <span className="text-blue">30%</span> OFF</h1>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae magnam officiis, facere, magni minus id sed voluptates illum praesentium laborum fugit quibusdam!</p>
 
-                  <div className="flex gap-4 mt-6">
+                  <div className="flex gap-6 mt-6">
                     <a href="" className="btn btn-blue">Purchase</a>
                     <a href="" className="btn btn-outline-blue">View Details</a>
                   </div>
