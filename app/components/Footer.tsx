2299e53e6d4514a538cee29556f459e1a51cc7b2
paulnyabaro <paulnyabaro1@gmail.com>
2024-02-07 10:51:06 +0300

    Working on footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer>
         <section className="main-footer">
             <div className="container">
-                <div className="d-flex">
+                <div className="flex gap-16">
                     <div className="w-full md:w-1/2 lg:w-1/4">
                         <img src="/img/logo-light.png" width="180px" alt="" />
                         <p className='mt-2 mb-4'>Affordable clinical and supportive care at home to allievate the economic and emotional costs of longterm hospital stays for patients and their families.</p>
@@ -22,6 +22,16 @@ const Footer = () => {
                             <a href="https://www.youtube.com/@benacare8469" className="footer-social-icon icn-youtube" target="_blank"><i className="bi bi-youtube"></i>
                             </a>
                         </span>
+                    </div>
+                    <div className="w-full md:w-1/2 lg:w-1/4">
+                        <h3>Quick Links</h3>
+                        <ul className="footer-links">
+                            <li><a href="">How We Do It</a></li>
+                            <li><a href="">About Us</a></li>
+                            <li><a href="">Home Equipment Shop</a></li>
+                            <li><a href="">Research & Development</a></li>
+                            <li><a href="">Work With Us</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
